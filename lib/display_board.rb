40e3ac393917d2c20fb24a41fe2442cfac02effb
Albert Zhao <albertzhao629@gmail.com>
2018-06-11 18:05:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,5 +8,5 @@ def display_board(board)
   puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 
-x = [" X " , " X " , " X " , " X " , " X " , " X " , " X " , " X " , " X "]
+x = ["  " , "  " , "  " , "  " , "  " , "  " , "  " , "  " , "  "]
 display_board(x)
